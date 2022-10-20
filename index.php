fce204838fd4714f893c3206494ea93cf4d5f8f6
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-10-20 20:17:58 +0200

    SQL query display works

--- index.php
+++ index.php
@@ -2,36 +2,43 @@
 require_once "vendor/autoload.php";
 
 use iutnc\location\db\ConnectionFactory;
-//require_once "src/classes/db/AccessData.php";
-/*
-try {
-    $pdo = new PDO('mysql:host=localhost;dbname=s3_location', 'root', '');
-}
-catch (PDOException $e)
-{
-    exit('Database error.');
-}*/
 
 ConnectionFactory::setConfig('./config/db.config.ini');
 ConnectionFactory::makeConnection();
 
 $rend = "";
 $action = $_GET['action'] ?? "";
+$error = "";
 
 $ad = new \iutnc\location\db\AccessData();
 
 switch ($action)
 {
     case 'listVehic' :
-        if ($_SERVER['REQUEST_METHOD'] == "GET")
+        $method = $_SERVER['REQUEST_METHOD'];
+        $checkfields = (!isset($_POST['categ'], $_POST['startD'], $_POST['endD']) || ($_POST['categ'] == "" || $_POST['startD'] == "" || $_POST['endD'] == "")) && $method == "POST";
+        if ($method == "GET" || $checkfields)
+        {
+            $error = $checkfields ? "Veuillez remplir tous les champs !" : "";
+
             $rend .= <<<END
             <form method='post' action='?action=listVehic'>
-                Catégorie : <input type='email' name='mail'>
-                Date de début : <input type='number' name='age'>
-                Date de fin : <input type='text' name='genre'>
+                Catégorie : <input type='text' name='categ'>
+                Date de début : <input type='date' name='startD'>
+                Date de fin : <input type='date' name='endD'>
                 <input type='submit' value='Valider'>
             </form>
+            <small>$error</small>
             END;
+        }
+
+        else
+            //$rend .= $ad->listVehic($_POST['categ'], $_POST['startD'], $_POST['endD']);
+            $rend .= $ad->testQuery();
+
+        $test = $_POST['categ'] != "" ? "vrai" : "faux";
+        echo $test;
+
 
         break;
     case 'majCal':
@@ -46,30 +53,33 @@ switch ($action)
         $rend .= "Bienvenue !";
 }
 
+echo <<<END
+    <!doctype html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>SQL - Location Project</title>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><a href="./">Accueil</a></li>
+                <li><a href="./?action=listVehic">List of available vehicles</a></li>
+                <li><a href="./?action=majCal">MAJ booking calendar</a></li>
+                <li><a href="./?action=locAmount">Locations amount</a></li>
+                <li><a href="./?action=categAgencies">Agencies with all vehicles' categories</a></li>
+                <li><a href="./?action=cliModels">Clients who have located 2 different models</a></li>
+            </ul>
+        </nav>
+        <main>
+            $rend
+        </main>
+    </body>
+    </html>
+END;
+
 ?>
 
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>SQL - Location Project</title>
-</head>
-<body>
-    <nav>
-        <ul>
-            <li><a href="./">Accueil</a></li>
-            <li><a href="./?action=listVehic">Ajouter un utilisateur</a></li>
-            <li><a href="./?action=majCal">Ajouter une playlist</a></li>
-            <li><a href="./?action=locAmount">Ajouter un podcast</a></li>
-            <li><a href="./?action=categAgencies">Ajouter un podcast</a></li>
-            <li><a href="./?action=cliModels">Ajouter un podcast</a></li>
-        </ul>
-    </nav>
-    <main>
-        <?php echo $rend; ?>
-    </main>
-</body>
-</html>
--- src/classes/db/AccessData.php
+++ src/classes/db/AccessData.php
@@ -6,6 +6,30 @@ class AccessData
 
     public function listVehic(string $categ, string $stDate, string $endDate) : string
     {
+        $res = "";
+        //return "ok";
+        $pdo = ConnectionFactory::makeConnection();
+        $query = <<<END
+            SELECT distinct Vehicule.no_imm, Vehicule.modele FROM Vehicule, Dossier
+            WHERE code_categ = ?
+                AND (date_retrait < ? OR dossier.date_retour > ?)
+                AND Vehicule.no_imm = Dossier.no_imm
+            END;
+
+        $st = $pdo->prepare($query);
+        $st->bindValue(1, $categ);
+        $st->bindValue(2, $stDate);
+        $st->bindValue(3, $endDate);
+        $st->execute();
+
+        foreach ($st->fetchAll(\PDO::FETCH_ASSOC) as $row)
+        {
+            $res .= \PDOStatement::rowCount($st);
+            for ($i = 0; $i < \PDOStatement::rowCount($st) ; $i ++)
+                $res .= "i: " . $row[$i];
+            $res .= "<br>";
+        }
+        /*
         $this.pst = this.co.prepareStatement("SELECT distinct Vehicule.no_imm, Vehicule.modele \n" +
                 "FROM Vehicule, Dossier\n" +
                 "WHERE code_categ = ?\n" +
@@ -15,5 +39,30 @@ class AccessData
 
         this.pstSet(pst, new String[]{categ, stDate, endDate});
         return this.displayPst();
+        */
+        return $res;
+    }
+
+    public function pstSet(\PDOStatement $rs, array $params) : void
+    {
+        for ($i = 0 ; $i < sizeof($params) ; $i ++)
+            $rs->bindParam($i + 1, $params[$i]);
+    }
+
+    public function testQuery ()
+    {
+        $res = "";
+        $table = "vehicule";
+        $db = ConnectionFactory::makeConnection();
+        $st = $db->prepare("SELECT * FROM vehicule");
+        //$st->bindParam(1, $table);
+        $st->execute();
+
+        return $st->columnCount();
+        /*while ($row = $st->fetch(\PDO::FETCH_ASSOC)) {
+            for ($i = 0; $i < $st->rowCount() ; $i ++)
+                $res .= "i: " . $row[$i + 1];
+        }*/
+        return $res;
     }
 }
