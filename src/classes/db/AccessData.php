d4bee6e98646a7ba108fb12b28511c9a84a2721e
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-10-20 21:41:59 +0200

    cleaning code

--- src/classes/db/AccessData.php
+++ src/classes/db/AccessData.php
@@ -5,45 +5,9 @@ class AccessData
 {
     private \PDOStatement $st;
 
-    public function listVehic_old(string $categ, string $stDate, string $endDate) : string
-    {
-        $res = "";
-        //return "ok";
-        $pdo = ConnectionFactory::makeConnection();
-        $query = <<<END
-            SELECT distinct Vehicule.no_imm, Vehicule.modele FROM Vehicule, Dossier
-            WHERE code_categ = ?
-                AND (date_retrait < ? OR dossier.date_retour > ?)
-                AND Vehicule.no_imm = Dossier.no_imm
-            END;
-
-        $st = $pdo->prepare($query);
-        $st->bindValue(1, $categ);
-        $st->bindValue(2, $stDate);
-        $st->bindValue(3, $endDate);
-        $st->execute();
-
-        foreach ($st->fetchAll(\PDO::FETCH_ASSOC) as $row)
-        {
-            $res .= \PDOStatement::rowCount($st);
-            for ($i = 0; $i < \PDOStatement::rowCount($st) ; $i ++)
-                $res .= "i: " . $row[$i];
-            $res .= "<br>";
-        }
-        /*
-        $this.pst = this.co.prepareStatement("SELECT distinct Vehicule.no_imm, Vehicule.modele \n" +
-                "FROM Vehicule, Dossier\n" +
-                "WHERE code_categ = ?\n" +
-                "    AND (date_retrait < ? OR dossier.date_retour > ?)\n" +
-                "    AND Vehicule.no_imm = Dossier.no_imm",
-                TYPE, MODE);
-
-        this.pstSet(pst, new String[]{categ, stDate, endDate});
-        return this.displayPst();
-        */
-        return $res;
-    }
-
+    /*
+     * --- QUERIES METHODS ---
+     */
     public function listVehic(string $categ, string $stDate, string $endDate) : string
     {
         $query = <<<END
@@ -55,15 +19,9 @@ class AccessData
 
         $db = ConnectionFactory::makeConnection();
         $this->st = $db->prepare($query);
-        /*
-        $this->st->bindValue(1, $categ);
-        $this->st->bindValue(2, $stDate);
-        $this->st->bindValue(3, $endDate);
-        */
         $this->stSet([$categ, $stDate, $endDate]);
         $this->st->execute();
 
-        //return $st->columnCount();
         return $this->display();
     }
 
@@ -83,7 +41,10 @@ class AccessData
         return $this->display();
     }
 
-    public function display(): string
+    /*
+     * --- BACKGROUND METHODS ---
+     */
+    private function display(): string
     {
         $res = "";
         while ($row = $this->st->fetch(\PDO::FETCH_NUM)) {
@@ -94,7 +55,7 @@ class AccessData
         return $res;
     }
 
-    public function stSet (array $params): void
+    private function stSet (array $params): void
     {
         for ($i = 0 ; $i < sizeof($params) ; $i ++)
             $this->st->bindValue($i + 1, $params[$i]);
